74cf4519061ccaddd8ad93ae8f9799100142fe18
ogoogo <>
2021-05-13 00:58:41 +0900

    連打アプリ発展

--- app/src/main/java/app/ogoshi/pico/renda/MainActivity.kt
+++ app/src/main/java/app/ogoshi/pico/renda/MainActivity.kt
@@ -2,18 +2,47 @@ package app.ogoshi.pico.renda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var second = 10
+
     var tapCount:Int =0
+
+    val timer :CountDownTimer = object : CountDownTimer(10000, 1000){
+        override fun onFinish() {
+            startButton.isVisible = true
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle_glay)
+            second = 10
+            tapCount=0
+        }
+
+        override fun onTick(millisUnitilFinished: Long) {
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle)
+            second -= 1
+            secondText.text=second.toString()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tapButton.setOnClickListener {
-            tapCount += 1
+        startButton.setOnClickListener {
             countText.text = tapCount.toString()
+            startButton.isVisible=false
+            timer.start()
+        }
+
+        tapButton.setOnClickListener {
+
+            if (second<10){
+                tapCount += 1
+                countText.text = tapCount.toString()
+            }
 
         }
 
